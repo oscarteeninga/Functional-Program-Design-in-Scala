528beab361be3ceadc7c980c3a42c8aa8b55c7ab
Oscar Teeninga <oscarteeninga@gmail.com>
2020-07-06 16:04:32 +0200

    Begin week 1

---
+++ week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,23 @@
+package quickcheck
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  lazy val genHeap: Gen[H] = oneOf(
+    const(Map.empty[Int, Int]),
+    for {
+
+    } yield
+  )
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+}
