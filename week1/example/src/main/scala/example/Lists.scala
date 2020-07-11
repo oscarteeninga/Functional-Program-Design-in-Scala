21889828edccf4cbec5ddd79b8c943b48ef4d98e
osacrteeninga <45202578+oscarteeninga@users.noreply.github.com>
2020-07-11 22:51:53 +0200

    xd

---
+++ week1/example/src/main/scala/example/Lists.scala
@@ -0,0 +1,51 @@
+package example
+
+object Lists {
+
+  /**
+   * This method computes the sum of all elements in the list xs. There are
+   * multiple techniques that can be used for implementing this method, and
+   * you will learn during the class.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+  def sum(xs: List[Int]): Int = xs match {
+    case Nil => 0
+    case x :: xs => x + sum(xs)
+  }
+
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+  def max(xs: List[Int]): Int = {
+    def maxAcc(ys: List[Int], acc: Int): Int = ys match {
+      case y :: ys => if (y > acc) maxAcc(ys, y) else maxAcc(ys, acc)
+      case Nil => acc
+    }
+    if (xs.nonEmpty) maxAcc(xs.tail, xs.head)
+    else throw new NoSuchElementException("There is no max value for empty list")
+  }
+}
---
+++ week1/example/src/main/scala/example/Playground.sc
@@ -0,0 +1,5 @@
+case class Person(firstName: String, lastName: String, age: Int)
+
+val p1 = Person("Oscar", "Teeninga", 22)
+
+
